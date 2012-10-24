d5dff851e44913b45e0502296b3a5f7ef1f71eb1
RonaldOlsthoorn <ronaldolsthoorn1@gmail.com>
2012-10-24 16:38:20 +0200

    update

--- Neural Networks/src/gui/MainFrame.java
+++ Neural Networks/src/gui/MainFrame.java
@@ -1,5 +1,24 @@
 package gui;
 
-public class MainFrame {
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	public static MainFrame main;
+	
+	public static MainFrame getMainFrame(){
+		
+		if(main == null){
+			
+			return new MainFrame();
+		}
+		
+		return main;
+	}
+	
+	private MainFrame(){
+		
+		
+	}
 
 }
