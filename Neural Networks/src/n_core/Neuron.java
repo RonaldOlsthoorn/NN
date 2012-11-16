c80e40af177e07a8c14eae22f6acea29a6382669
RonaldOlsthoorn <ronaldolsthoorn1@gmail.com>
2012-11-16 13:53:17 +0100

    new class variable (to be implemented yet)

--- Neural Networks/src/n_core/Neuron.java
+++ Neural Networks/src/n_core/Neuron.java
@@ -1,5 +1,25 @@
 package n_core;
+import java.util.HashMap;
 
 public class Neuron {
+	
+	private HashMap<String,Neuron> inputNeurons;
+	private HashMap<String,Neuron> outputNeurons;
+	private HashMap<String,Variable> variants;
+	
+	
+	public Neuron(){
+		
+		
+	}
+	
+	public double evaluate(){
+		
+		return 0;
+	}
+	
+	public void update(){
+		
+	}
 
 }
